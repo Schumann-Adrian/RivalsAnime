abd772bb2e98bd9d437f737cb69ffa8053312257
adria <adria@DESKTOP-500BSDC>
2026-04-29 15:43:18 +0200

    Agregar archivos de proyecto.

---
+++ RivalsAnime/Class1.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RivalsAnime
+{
+    internal class Conexion
+    {
+        private string servidor = "localhost";
+        private string bd = "batallaanimedb";
+        private string usuario = "root"; // Usuario por defecto de HeidiSQL
+        private string password = "123456789";
+
+        public MySqlConnection establecerConexion()
+        {
+            string cadenaconexion = $"server={servidor};database={bd};uid={usuario};pwd={password};";
+            MySqlConnection conexion = new MySqlConnection(cadenaconexion);
+
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error al conectar a la base de datos: " + e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ RivalsAnime/LoginForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace RivalsAnime
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(248, -14);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(323, 127);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 445);
+            Controls.Add(pictureBox1);
+            DoubleBuffered = true;
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+    }
+}
